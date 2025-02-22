a6a5045ebc87e638e474cc94320cd42f9eac4941
Gaspo <gasparh@gmail.com>
2025-02-22 00:20:35 -0300

    0'o

--- P1_Cuadros 2_Primeros cruces.R
+++ P1_Cuadros 2_Primeros cruces.R
@@ -4,8 +4,12 @@ C_niveled_estab <- base  %>% filter(ESTADO==1 &  CAT_OCUP==3)  %>%
                       x="nivel.ed",
                       y="tamanio.establec.nueva",
                       weights="PONDERA")
-Csexo_niveled_estab <- base  %>% filter(ESTADO==1 &  CAT_OCUP==3)  %>%
-  group_by(CH04) %>%
+Cvar_niveled_estab <- base  %>% filter(ESTADO==1 &  CAT_OCUP==3 & CH04==1)  %>%
+  calculate_tabulates(base = base, 
+                      x="nivel.ed",
+                y="tamanio.establec.nueva",
+                      weights="PONDERA")
+Cmuj_niveled_estab <- base  %>% filter(ESTADO==1 &  CAT_OCUP==3 & CH04==2)  %>%
   calculate_tabulates(base = base, 
                       x="nivel.ed",
                       y="tamanio.establec.nueva",
