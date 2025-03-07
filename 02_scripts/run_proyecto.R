bbc69dc62679cd67ef6386df8a3d8c9bccfc4149
agustoromero <agus.romer.98@gmail.com>
2025-03-06 22:21:59 -0300

    actualizacion run_proyecto

--- 02_scripts/run_proyecto.R
+++ 02_scripts/run_proyecto.R
@@ -1,19 +1,23 @@
-# 🚀 Script maestro para ejecutar todo el flujo de análisis de precariedad laboral
-source("run_proyecto.R")
-# 1️⃣ Cargar datos
-source("scripts/01_carga_datos_y_librerias.R")
+# Ejecutar los scripts en orden
 
-# 2️⃣ Generar variables sociodemográficas
-source("scripts/02_variables_persona.R")
+# Carpeta 01_modificaciones a la base
+source("02_scripts/01_modificaciones_a_la_base/01_carga_datos_y_librerias.R")
+source("02_scripts/01_modificaciones_a_la_base/01_carga_datos_y_librerias.R")
+source("02_scripts/01_modificaciones_a_la_base/02_variables_persona.R")
+source("02_scripts/01_modificaciones_a_la_base/03_ocupados.R")
+source("02_scripts/01_modificaciones_a_la_base/04_asalariados.R")
+source("02_scripts/01_modificaciones_a_la_base/05_precariedad.R")
 
-# 3️⃣ Filtrar ocupados
-source("scripts/03_ocupados.R")
+# Carpeta 02_funciones
+source("02_scripts/02_funciones/01_c_estado_cond.act_sexo.R")
+source("02_scripts/02_funciones/02_c_desocupados.R")
+source("02_scripts/02_funciones/03_c_rama_tamanio_sexo.R")
+source("02_scripts/02_funciones/04_c_tamanio.nueva_nivel_ed_rama_sexo.R")
+source("02_scripts/02_funciones/05_c_rama_cond.registro_tipo.establecimiento.R")
+source("02_scripts/02_funciones/06_c_estab_condicion_registro_tipo.R")
+source("02_scripts/02_funciones/07_c_rama_condicion_registro.R")
+source("02_scripts/02_funciones/08_c_signos_precariedad.R")
+source("02_scripts/02_funciones/09_preca_tecno_calif_sexo_NIVEL_ED.R")
+source("02_scripts/02_funciones/091_preca_sindescuento_sexo_niveled.R")
 
-# 4️⃣ Filtrar asalariados y agregar variables laborales
-source("scripts/04_asalariados.R")
-
-# 5️⃣ Calcular precariedad
-source("scripts/05_precariedad.R")
-
-# 🏁 Mensaje final
-message("✅ Flujo completo ejecutado con éxito. Base lista para análisis en RMarkdown.")
+print("Ejecución completa de todos los scripts.")
