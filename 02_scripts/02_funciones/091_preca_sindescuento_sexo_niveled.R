ea4cf9b0479110fddcea7ad42f79128a8e33b23d
agustoromero <agus.romer.98@gmail.com>
2025-03-06 21:55:35 -0300

    numeracion cuadro

--- 02_scripts/02_funciones/091_preca_sindescuento_sexo_niveled.R
+++ 02_scripts/02_funciones/091_preca_sindescuento_sexo_niveled.R
@@ -1,17 +1,8 @@
-base <- base %>%  
-  filter(ESTADO == 1, CAT_OCUP == 3) %>%  # Ocupados asalariados
-  mutate(
-    preca_signo_sindescuento = case_when(
-      signo_sindescuento == 1 ~ 1,  # Si el signo de "sin descuento" es 1, marca como precario
-      TRUE ~ 0  # De lo contrario, no es precario
-    )
-  )
-
 # Analizar la precariedad signo_sindescuento por sexo
 tabla_preca_SS_sexo <- calculate_tabulates(
-  base = base,
+  base = base_asalariados,
   x = "CH04",  # Sexo (código 1 = varón, 2 = mujer)
-  y = "preca_signo_sindescuento",
+  y = "signo_sindescuento",
   weights = "PONDERA"
 )
 
@@ -19,35 +10,35 @@ print(tabla_preca_SS_sexo)
 
 # Analizar la precariedad signo_sindescuento por nivel educativo (ambos sexos)
 tabla_preca_SS_educ <- calculate_tabulates(
-  base = base,
+  base = base_asalariados,
   x = "NIVEL_ED",
-  y = "preca_signo_sindescuento",
+  y = "signo_sindescuento",
   weights = "PONDERA"
 ) %>%
   mutate(sexo = "Ambos")
 print(tabla_preca_SS_educ)
 
-tabla_precaSS_educ_varon <- base %>% 
+tabla_precaSS_educ_varon <- base_asalariados %>% 
   filter(CH04 == "1") %>%  # Solo varones
   calculate_tabulates(
     x = "NIVEL_ED",
-    y = "preca_signo_sindescuento",
+    y = "signo_sindescuento",
     weights = "PONDERA"
   ) %>% 
   mutate(sexo = "Varón")
 print(tabla_precaSS_educ_varon)
 
-tabla_precaSS_educ_mujer <- base %>% 
+tabla_precaSS_educ_mujer <- base_asalariados %>% 
   filter(CH04 == "2") %>%  # Solo mujeres
   calculate_tabulates(
     x = "NIVEL_ED",
-    y = "preca_signo_sindescuento",
+    y = "signo_sindescuento",
     weights = "PONDERA"
   ) %>% 
   mutate(sexo = "Mujer")
 print(tabla_precaSS_educ_mujer)
 
-tabla_precaSS_educ_sexo_final <- bind_rows(tabla_preca_SS_educ, tabla_precaSS_educ_varon, tabla_precaSS_educ_mujer)
+c.91_precaSS_educ_sexo_final <- bind_rows(tabla_preca_SS_educ, tabla_precaSS_educ_varon, tabla_precaSS_educ_mujer)
 
 # Limpiar el entorno de trabajo de las variables no necesarias
 objetos <- c("tabla_precaSS_educ_varon", "tabla_precaSS_educ_mujer", "tabla_preca_SS_sexo")
